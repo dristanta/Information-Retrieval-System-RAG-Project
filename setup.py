c2d5d3bf2174cb58783c78311fa4a886c04b4f42
Dristanta Nirola <dristantanirola@Dristantas-MacBook-Air.local>
2026-01-19 00:43:56 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Information-Retrieval-System-RAG-Project",
+    version="0.0.0",
+    author="Dristantanirola",
+    author_email="dristantanirola@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
